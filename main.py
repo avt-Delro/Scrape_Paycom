41ba58b9142c59284438bcf344bbbe5d5b4ebf79
avt-Delro <137831729+mbuncha@users.noreply.github.com>
2026-07-09 15:20:49 -0700

    To pull

--- main.py
+++ main.py
@@ -51,14 +51,14 @@ def main():
         if day_today.weekday() == 0:  # Check if it's Monday
             weekly_scraping.create_report_summary_we_month(paycom_filepath, None)
             weekly_scraping.create_clp_summary_we_month(clp_filepath, None)
-            weekly_scraping.send_email_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com',paycom_filepath)
-            weekly_scraping.send_email_missing_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com', missingpunches_filepath)
-            weekly_scraping.send_email_clp_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com', clp_filepath)
+            weekly_scraping.send_email_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com',paycom_filepath)
+            weekly_scraping.send_email_missing_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', missingpunches_filepath)
+            weekly_scraping.send_email_clp_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', clp_filepath)
 
         if datetoday.day == last_day:  # Check if it's the last day of the month
-            weekly_scraping.send_email_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com',paycom_filepath)
-            weekly_scraping.send_email_missing_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com', missingpunches_filepath)
-            weekly_scraping.send_email_clp_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com', clp_filepath)
+            weekly_scraping.send_email_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com',paycom_filepath)
+            weekly_scraping.send_email_missing_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com', missingpunches_filepath)
+            weekly_scraping.send_email_clp_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com', clp_filepath)
     except Exception as e:
         print(f"⚠️ Error processing message: {e}")
         error_type = type(e).__name__
@@ -70,6 +70,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # paycom_filepath = web_scraping.paycom_scraping('https://www.paycomonline.net/v4/cl/cl-login.php', paycom_user, paycom_pass, client_code, 1)
+    # missingpunches_filepath = web_scraping.paycom_scraping('https://www.paycomonline.net/v4/cl/cl-login.php', paycom_user, paycom_pass, client_code, 2)
+    # clp_filepath = web_scraping.paycom_scraping('https://www.paycomonline.net/v4/cl/cl-login.php', paycom_user, paycom_pass, client_code, 3)
+    # weekly_scraping.create_report_summary_we_month(paycom_filepath, None)
+    # weekly_scraping.create_clp_summary_we_month(clp_filepath, None)
+    # # # weekly_scraping.send_email_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com',paycom_filepath)
+    # # # weekly_scraping.send_email_missing_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', missingpunches_filepath)
+    # # # weekly_scraping.send_email_clp_we_month('raquel@avatco.com;mason@avatco.com;Frank.Shi@avatco.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', clp_filepath)
+    # weekly_scraping.create_report_summary_we_month(paycom_filepath, {'Schedule Group.1':[8606,8607,8608,8609,8610,8619,8620]})
+    # # weekly_scraping.create_clp_summary_we_month(clp_filepath, {'Schedule Group.1':[8606,8607,8608,8609,8610,8619,8620]})
+    # weekly_scraping.send_email_we_month('ever@americantirestores.com;leon@tireoutletus.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', paycom_filepath, 'Retail Stores')
+    # # weekly_scraping.send_email_we_month('vjdelrosario@avatco.com', 'vjdelrosario@avatco.com', paycom_filepath, 'Retail Stores')
+    # weekly_scraping.send_email_missing_we_month('ever@americantirestores.com;leon@tireoutletus.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', missingpunches_filepath, {'Schedule Group.1':[8606,8607,8608,8609,8610,8619,8620]}, 'Retail Stores')
+    # weekly_scraping.send_email_clp_we_month('ever@americantirestores.com;leon@tireoutletus.com', 'vjdelrosario@avatco.com;TTPhan@avatco.com', clp_filepath, 'Retail Stores') 
     
 
     
